927a3d2a5d3051b91bb5eb4d071b7d8f3b4c4a9c
esquizo ciborgue <nicolasnardi404@gmail.com>
2025-02-24 14:51:47 +0100

    add favicon

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
