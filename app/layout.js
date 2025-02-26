44e723d3aee0be5fbb8f3ad38a549e3cb1cd13ba
esquizo ciborgue <nicolasnardi404@gmail.com>
2025-02-26 14:28:18 +0100

    add analytics

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,15 @@
+import { Analytics } from "@vercel/analytics/react";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
